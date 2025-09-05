8449e657892faf1b35f363b4533ead1cbbaf44bc
JH <wngml0708@gmail.com>
2025-09-05 12:48:31 +0900

    임시 토큰 발급 ë api 삭제

--- src/main/java/com/mtcoding/minigram/users/UsersController.java
+++ src/main/java/com/mtcoding/minigram/users/UsersController.java
@@ -1,35 +1,11 @@
 package com.mtcoding.minigram.users;
 
-import com.mtcoding.minigram._core.util.JwtUtil;
-import com.mtcoding.minigram._core.util.Resp;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 public class UsersController {
     private final UserService userService;
-    private final HttpSession session;
 
-    // 임시 유저 토큰 발급 (1 : 관리자 / 2 : ssar (일반유저))
-    @PostMapping("/users/{userId}/token")
-    public ResponseEntity<?> tempLogin(@PathVariable Integer userId) {
-        User tempUser = User.builder()
-                .id(userId)
-                .username(userId == 1 ? "minigram" : "ssar")
-                .roles(userId == 1 ? "ADMIN" : "USER")
-                .build();
-
-        String accessToken = JwtUtil.create(tempUser);
-
-        Map<String, Object> body = Map.of("accessToken", accessToken);
-
-        return Resp.ok(body);
-    }
 }
