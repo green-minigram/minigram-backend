52431288c9fe8d24976bddde23901ccd71f058e9
JH <wngml0708@gmail.com>
2025-09-09 09:24:40 +0900

    어노테이션 수정

--- src/main/java/com/mtcoding/minigram/reports/reasons/ReportReasonRepository.java
+++ src/main/java/com/mtcoding/minigram/reports/reasons/ReportReasonRepository.java
@@ -1,16 +1,16 @@
 package com.mtcoding.minigram.reports.reasons;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Repository
 public class ReportReasonRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public Optional<ReportReason> findById(Integer id) {
         return Optional.ofNullable(em.find(ReportReason.class, id));
