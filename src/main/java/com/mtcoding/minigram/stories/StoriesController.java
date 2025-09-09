adbdec465003e704bc1230abe681b3aeb0406455
JH <127887466+jh0804@users.noreply.github.com>
2025-09-09 11:16:37 +0900

    [feat] 스토리 통합테스트 (#16)
    스토리 통합테스트 완료

--- src/main/java/com/mtcoding/minigram/stories/StoriesController.java
+++ src/main/java/com/mtcoding/minigram/stories/StoriesController.java
@@ -12,17 +12,17 @@ import org.springframework.web.bind.annotation.*;
 public class StoriesController {
     private final StoryService storyService;
 
-    // 로그인 유저의 스토리 목록 (최근 5개)
-    @GetMapping("/s/api/users/me/stories")
-    public ResponseEntity<?> findAll(@AuthenticationPrincipal User user) {
-        StoryResponse.ListDTO respDTO = storyService.findAllMyStories(user.getId());
-        return Resp.ok(respDTO);
-    }
-
     // 스토리 상세
     @GetMapping("/s/api/stories/{storyId}")
     public ResponseEntity<?> findByStoryId(@PathVariable Integer storyId, @AuthenticationPrincipal User user) {
         StoryResponse.DetailDTO respDTO = storyService.findByStoryId(storyId, user.getId());
+        return Resp.ok(respDTO);
+    }
+
+    // 로그인 유저의 스토리 목록 (최근 5개)
+    @GetMapping("/s/api/users/me/stories")
+    public ResponseEntity<?> findAllMyStories(@AuthenticationPrincipal User user) {
+        StoryResponse.ListDTO respDTO = storyService.findAllMyStories(user.getId());
         return Resp.ok(respDTO);
     }
 
