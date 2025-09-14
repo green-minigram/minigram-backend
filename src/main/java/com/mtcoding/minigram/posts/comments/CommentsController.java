71c5dc0fb991b3de8c57ac6f22e671aad8af6c56
jh0804 <wngml0708@gmail.com>
2025-09-15 02:55:32 +0900

    대댓글 목록 조회 api + 통합테스트 완료

--- src/main/java/com/mtcoding/minigram/posts/comments/CommentsController.java
+++ src/main/java/com/mtcoding/minigram/posts/comments/CommentsController.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CommentsController {
     private final CommentService commentService;
-    private final HttpSession session;
 
+    // 게시글 기준 댓글 전체 목록 조회
     @GetMapping("/s/api/posts/{postId}/comments")
     public ResponseEntity<?> findAllByPostId(@PathVariable Integer postId, @AuthenticationPrincipal User user) {
         CommentResponse.ListDTO respDTO = commentService.findAllByPostId(postId, user.getId());
@@ -26,6 +26,13 @@ public class CommentsController {
     @DeleteMapping("/s/api/comments/{commentId}")
     public ResponseEntity<?> delete(@PathVariable Integer commentId, @AuthenticationPrincipal User user) {
         CommentResponse.DeleteDTO respDTO = commentService.delete(commentId, user.getId(), user.getRoles());
+        return Resp.ok(respDTO);
+    }
+
+    // root 기준 대댓글 전체 목록 조회
+    @GetMapping("/s/api/comments/{commentId}/replies")
+    public ResponseEntity<?> findRepliesByRoot(@PathVariable Integer commentId, @AuthenticationPrincipal User user) {
+        CommentResponse.ListDTO respDTO = commentService.findRepliesByRoot(commentId, user.getId());
         return Resp.ok(respDTO);
     }
 }
